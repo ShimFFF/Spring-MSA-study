df8af39f4733c42d3fa01722fc34eda6a5756dc1
shimfff <shimseawon1510@gmail.com>
2024-08-18 02:46:25 +0900

    :sparkles: Feat: EurekaServer 설정

--- ecomerce/src/main/java/com/example/ecomerce/EcomerceApplication.java
+++ ecomerce/src/main/java/com/example/ecomerce/EcomerceApplication.java
@@ -2,7 +2,9 @@ package com.example.ecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // Eureka Server 설정하기 위한 어노테이션 추가
 @SpringBootApplication
 public class EcomerceApplication {
 
