484799a1f0a624eacdb1ff10c224fa5311ab4b61
shimfff <shimseawon1510@gmail.com>
2024-08-28 03:25:46 +0900

    :sparkles: Feat: GlobalFilter 추가

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,43 @@
+package com.example.gatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Global PRE filter: baseMessage -> {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global PRE filter request id -> {}", request.getId());
+            }
+
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Global POST filter: response code -> {}", response.getStatusCode());
+                }
+            }));
+        });
+    }
+
+
+    @Data
+    public static class Config { //해당 설정값은 application.yml에 설정해준다.
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
