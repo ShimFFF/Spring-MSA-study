e690fe2e58b882543f3c5662cc6d2ddf21eddd03
shimfff <shimseawon1510@gmail.com>
2024-08-28 03:16:04 +0900

    :sparkles: Feat: CustonFilter 추가

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
@@ -0,0 +1,36 @@
+package com.example.gatewayservice.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // 기능 설명: PRE filter와 POST filter를 구현한다.
+        // PRE filter: 요청이 들어오기 전에 실행되는 필터
+        // POST filter: 요청이 나가고 난 후에 실행되는 필터
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter: request id -> {}", request.getId());
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+            }));
+        });
+    }
+
+    public static class Config {
+        // Put the configuration properties
+    }
+
+}
