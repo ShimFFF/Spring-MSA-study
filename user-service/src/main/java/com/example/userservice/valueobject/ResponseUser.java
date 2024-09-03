154ea137a83bf16f7eb44225052b99f1bccb80be
shimfff <shimseawon1510@gmail.com>
2024-09-03 21:33:10 +0900

    :sparkles: Feat: import 추가

--- user-service/src/main/java/com/example/userservice/valueobject/ResponseUser.java
+++ user-service/src/main/java/com/example/userservice/valueobject/ResponseUser.java
@@ -3,6 +3,8 @@ package com.example.userservice.valueobject;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 // null 값은 제외하고 json으로 변환
