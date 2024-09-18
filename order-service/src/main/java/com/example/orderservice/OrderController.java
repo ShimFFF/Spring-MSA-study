ae9fd01535509bdd072eaa807d897187260bc9f6
shimfff <shimseawon1510@gmail.com>
2024-09-18 17:31:40 +0900

    :sparkles: Feat: order 서비스 완성

--- order-service/src/main/java/com/example/orderservice/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/OrderController.java
@@ -1,6 +1,8 @@
 package com.example.orderservice;
 
 import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,10 @@ public class OrderController {
     private final OrderService orderService;
     private final Environment env;
 
+    private final KafkaProducer kafkaProducer;
+
+    private final OrderProducer orderProducer;
+
     @GetMapping("/health_check")
     public String status() {
         return String.format("It's Working in Order Service on LOCAL PORT %s (SERVER PORT %s)",
@@ -45,6 +51,14 @@ public class OrderController {
         /* jpa */
         OrderDto createdOrder = orderService.createOrder(orderDto);
         ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        /* kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+
+        /* send this order to the kafka */
+        kafkaProducer.send("example-catalog-topic", orderDto);
+
         log.info("After added orders data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
@@ -58,6 +72,17 @@ public class OrderController {
         orderList.forEach(v -> {
             result.add(new ModelMapper().map(v, ResponseOrder.class));
         });
+
+//        try {
+//            Random rnd = new Random();
+//            int value = rnd.nextInt(5);
+//            if (value % 2 == 0) {
+//                Thread.sleep(10000);
+//                throw new Exception("장애 발생");
+//            }
+//        } catch(InterruptedException ex) {
+//            log.warn(ex.getMessage());
+//        }
 
         log.info("Add retrieved orders data");
 
