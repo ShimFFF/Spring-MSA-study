c7c91d84a2c40e77e2aa6dbbb1f744d9964b0e10
shimfff <shimseawon1510@gmail.com>
2024-10-16 22:31:59 +0900

    :memo: Docs: FeignErrorDecoder 구현 설명 추석 추가

--- user-service/src/main/java/com/example/userservice/error/FeignErrorDecoder.java
+++ user-service/src/main/java/com/example/userservice/error/FeignErrorDecoder.java
@@ -10,19 +10,22 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
-    Environment env;
+    Environment env; //환경 변수 사용
 
-    @Autowired
+    @Autowired //의존성 주입 -> FeignErrorDecoder 클래스를 Spring 빈으로 등록
     public FeignErrorDecoder(Environment env) {
         this.env = env;
     }
 
-    @Override
+    @Override // ErrorDecoder 인터페이스의 decode 메소드 구현
+    // FeignClient를 통해 호출한 서비스의 응답 코드에 따라 예외 처리
+    //HTTP 응답 코드에 따라 예외 처리
     public Exception decode(String methodKey, Response response) {
         switch(response.status()) {
             case 400:
                 break;
             case 404:
+                // getOrders 메소드 호출 시에 발생한 404 에러라면
                 if (methodKey.contains("getOrders")) {
                     return new ResponseStatusException(HttpStatus.valueOf(response.status()),
                             "User's orders is empty");
